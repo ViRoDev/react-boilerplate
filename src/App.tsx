333c67add1765b9ae9488d3cab1c57c56c240626
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-08-30 22:29:29 +0300

    created lightweight esbuild/react/typescript boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom/client"
+
+export const App = () => 
+	{
+	console.log("hehe");
+	return (
+		<div>
+			<div className="header"/>
+			<div className="body">
+				Hello, React!
+			</div>
+		</div>
+	)
+}
